f888a3b4707a51ac5b6cc54670a92bf325a8fb98
intisarosman1 <77607212+intisarosman1@users.noreply.github.com>
2023-05-03 19:52:11 -0700

    Shiba Inu

---
+++ Shiba/app/src/main/java/com/example/shiba/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.shiba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+
+class gMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val food = findViewById<Button>(R.id.button)
+        food.text = "Food" 
+
+        val touch = findViewById<Button>(R.id.button2)
+        touch.text = "Touch"
+
+        val image = findViewById<ImageView>(R.id.imageView)
+
+        food.setOnClickListener {
+            image.setBackgroundResource(R.drawable.calm)
+            val toast = Toast.makeText(applicationContext, "Calm Shiba Inu!", Toast.LENGTH_SHORT)
+            toast.show()
+
+        }
+
+        touch.setOnClickListener {
+            image.setBackgroundResource(R.drawable.angry)
+            val toast = Toast.makeText(applicationContext, "Angry Shiba Inu!", Toast.LENGTH_SHORT)
+            toast.show()
+
+        }
+    }
+}
